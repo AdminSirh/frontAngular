ae33b1061c4050f1a3244c15580c9179af6dd18a
RicardoRoscero <101841397+RicardoRoscero@users.noreply.github.com>
2024-09-13 17:59:42 -0600

    Redirección a signin si expira el token

--- src/services/auth.interceptor.ts
+++ src/services/auth.interceptor.ts
@@ -1,21 +1,37 @@
-import { LoginService } from './login.service';
-import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HttpErrorResponse, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { LoginService } from './login.service';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
-  constructor(private loginService: LoginService) {}
+  constructor(
+    private loginService: LoginService,
+    private router: Router
+  ) {}
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     let authReq = req;
     const token = this.loginService.getToken();
-    if (token != null) {
+    if (token) {
       authReq = authReq.clone({
         setHeaders: { Authorization: `Bearer ${token}` }
       });
     }
-    return next.handle(authReq);
+    /*Si el error es el 401 siginfica que el token del usuario ha expirado, se redirige a login
+    El tiempo de expiración es de 30 min, en la clase JwtUtils del backend (crearToken())*/
+    return next.handle(authReq).pipe(
+      catchError((error: HttpErrorResponse) => {
+        if (error.status === 401) {
+          // Token inválido o expirado; cerrar sesión y redirigir al usuario al login
+          this.loginService.logout();
+          this.router.navigate(['/auth/signin']);
+        }
+        return throwError(error);
+      })
+    );
   }
 }
 
