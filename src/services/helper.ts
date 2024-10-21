bac43bd264537ed8a9ca93c99f330cf3eadda294
Osvaldo13 <ozzval2.guerrero13@homtail.com>
2024-10-21 10:16:22 -0600

    Auto stash before merge of "master" and "origin/master"

--- src/services/helper.ts
+++ src/services/helper.ts
@@ -1,5 +1,9 @@
+<<<<<<< Updated upstream
 //Para el proyecto en local
 let baseUrl = 'http://localhost:8080';
 //Para el proyecto en el servidor
 //let baseUrl = 'http://10.19.1.242:8080/backB';
+=======
+let baseUrl = 'http://10.19.1.242:8080/backB';
+>>>>>>> Stashed changes
 export default baseUrl;
