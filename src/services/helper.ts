2cdbaae280b1bd7dc055e040ca5805c310a64497
Osvaldo13 <ozzval2.guerrero13@homtail.com>
2024-10-21 10:18:22 -0600

    correcion helper

--- src/services/helper.ts
+++ src/services/helper.ts
@@ -1,9 +1,6 @@
-<<<<<<< Updated upstream
+
 //Para el proyecto en local
 let baseUrl = 'http://localhost:8080';
 //Para el proyecto en el servidor
-//let baseUrl = 'http://10.19.1.242:8080/backB';
-=======
-let baseUrl = 'http://10.19.1.242:8080/backB';
->>>>>>> Stashed changes
+//let baseUrl = 'http://10.19.1.242:8080/backB';                                                 
 export default baseUrl;
